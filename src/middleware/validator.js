da24b02469e1b1bdf82dd369d92abeb565ee0d60
ahmadhelwa <AHM0173377@ju.edu.jo>
2022-06-02 14:51:17 +0300

    solved

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -3,15 +3,19 @@ function validator() {
   return (req, res, next) => {
     let regx = /^[a-zA-Z]+$/;
 
-    if (req !== "") {
-      next("error");
-    } else if (regx.test(req.query.name)) {
-      next();
-    } else if (req.query.name === "") {
-      next("name is empty");
-    } else if (!regx.test(req.query.name)) {
-      next(" should be a valid string");
+    if (req.query.name) {
+      if (regx.test(req.query.name)) {
+        next();
+      } else if (!regx.test(req.query.name)) {
+        next(" should be a valid string");
+      }
+    } else {
+      next("name not provide");
     }
+
+    // if(req.query.name =="")
+    //   {next("error")}
   };
 }
+
 module.exports = validator;
